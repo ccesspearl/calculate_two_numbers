9308a3484d41154c29762e760f6ccb191a17ad86
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-20 17:43:08 +0800

    first try coding the exercise 1

--- calculate_twonumbers.py
+++ calculate_twonumbers.py
@@ -1,2 +1,16 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
 # Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+def product_or_sum():
+    first = (int(input("Enter first number: ")))
+    second = (int(input("Enter second number: ")))
+
+    product = first * second 
+    total_sum = first + second 
+    
+    if product <= 1000:
+        print("The result is ", product) 
+    else: 
+        print("The result is ",total_sum) 
+
+product_or_sum()
